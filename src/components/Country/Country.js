41d13fb6ecb8723a780319c6f741220d3c21f910
Dev-Helal <myprofile274@gmail.com>
2023-01-30 11:39:51 +0600

    Country Subregion Added

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Country.css'
 
 const Country = (props) => {
-    const {area, region, name , population,flags,capital,languages} = props.country;
+    const {area, region, name , population,flags,capital,languages,subregion} = props.country;
     console.log(props.country)
     return (
             <div className='country'>
@@ -12,7 +12,7 @@ const Country = (props) => {
                 <p>Population : {population}</p>
                 <p>Region : {region}</p>
                 <img src={flags.png} alt="" />
-                <p>Language : {languages ? languages : 'No Data Found'}</p>
+                <p>Subregion : {subregion}</p>
             </div>
     );
 };
