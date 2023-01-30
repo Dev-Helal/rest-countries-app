6bc2992a525192adabe65f9e3b771c7b622dfa76
Dev-Helal <myprofile274@gmail.com>
2023-01-30 11:34:27 +0600

    Capital Name Added

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './Country.css'
 
 const Country = (props) => {
-    const {area, region, name , population,flags} = props.country;
-    // console.log(props.country)
+    const {area, region, name , population,flags,capital} = props.country;
+    console.log(props.country)
     return (
             <div className='country'>
                 <p>Area : {area}</p>
                 <h2>Country Name : {name.common}</h2>
+                <h4>Capital : {capital}</h4>
                 <p>Population : {population}</p>
                 <p>Region : {region}</p>
                 <img src={flags.png} alt="" />
